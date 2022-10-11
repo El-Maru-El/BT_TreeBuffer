3b6a04bfa8a9b0bf89bec933744b2a94cf9eabc1
Maruan El-Mahgary <maruan.el-mahgary@gmx.de>
2022-10-11 17:33:19 +0200

    Some more structure + only remember timestamps for all files

--- current_implementation/new_buffer_tree.py
+++ current_implementation/new_buffer_tree.py
@@ -4,7 +4,7 @@ from itertools import chain
 
 from current_implementation.constants_and_helpers import *
 from collections import namedtuple
-
+import csv
 
 ChildParent = namedtuple('ChildParent', field_names=['child', 'parent'])
 
@@ -30,7 +30,7 @@ class BufferTree:
 
         self.total_written_leaf_blocks = 0
         root_node = TreeNode(is_internal_node=False, handles=[], children=[], buffer_blocks=[])
-        self.root = root_node.dir_path
+        self.root = root_node.node_timestamp
         self.tree_buffer = TreeBuffer(max_size=self.B)
         self.internal_node_emptying_queue = []
         self.leaf_node_emptying_queue = []
@@ -53,13 +53,15 @@ class BufferTree:
             # TODO This could be done more efficiently, since this Block is first written then immediately read again.
             root = load_node(self.tree_buffer)
             root.add_block_to_buffer(self.tree_buffer.get_elements())
+            root.last_buffer_size = self.b
+
             self.tree_buffer.clear_elements()
 
             if root.buffer_is_full():
                 if root.is_internal_node():
-                    self.internal_node_emptying_queue.append(ChildParent(root.dir_path, None))
+                    self.internal_node_emptying_queue.append(ChildParent(root.node_timestamp, None))
                 else:
-                    self.leaf_node_emptying_queue.append(ChildParent(root.dir_path, None))
+                    self.leaf_node_emptying_queue.append(ChildParent(root.node_timestamp, None))
                 self.clear_all_full_buffers()
 
     def clear_all_full_buffers(self):
@@ -89,22 +91,20 @@ class BufferTree:
 
 
 class TreeNode:
-    def __init__(self, dir_path=None, is_internal_node=None, handles=None, children=None, buffer_blocks=None):
-        if dir_path is None:
-            dir_path = generate_new_nodes_dir()
+    def __init__(self, node_timestamp=None, is_internal_node=None, handles=None, children=None, buffer_blocks=None, last_buffer_size=0):
+        if node_timestamp is None:
+            node_timestamp = generate_new_nodes_dir()
 
         if buffer_blocks is None:
             buffer_blocks = []
 
-        self.dir_path = dir_path
+        self.node_timestamp = node_timestamp
 
         self.handles = handles
         self.children_paths = children
         self.is_intern = is_internal_node
         self.buffer_blocks = buffer_blocks
-
-    def get_buffer_dir(self):
-        return get_buffer_dir_for_node(self.dir_path)
+        self.last_buffer_size = last_buffer_size
 
     def is_internal_node(self):
         if self.is_intern is None:
@@ -113,9 +113,10 @@ class TreeNode:
         return self.is_intern
 
     def add_block_to_buffer(self, elements):
-        buffer_file_name = generate_buffer_file_name()
-        file_path = get_buffer_dir_for_node(self.dir_path)
-        self.buffer_blocks.append(buffer_file_name)
+        buffer_timestamp = get_current_timestamp()
+        file_path = get_buffer_file_path_from_timestamps(self.node_timestamp, buffer_timestamp)
+
+        self.buffer_blocks.append(buffer_timestamp)
         write_buffer_block(file_path, elements)
 
     def add_elements_to_buffer(self, parent_path, elements):
@@ -126,10 +127,11 @@ class TreeNode:
         # TODO append to that buffer until it is full, partition the other elements to other new buffers
 
         if self.buffer_is_full():
+            # TODO We need to check first whether the node might already be in the queue
             if self.is_internal_node():
-                tree.internal_node_emptying_queue.insert(0, ChildParent(self.dir_path, parent_path))
+                tree.internal_node_emptying_queue.insert(0, ChildParent(self.node_timestamp, parent_path))
             else:
-                tree.leaf_node_emptying_queue.append(ChildParent(self.dir_path, parent_path))
+                tree.leaf_node_emptying_queue.append(ChildParent(self.node_timestamp, parent_path))
 
     def buffer_is_full(self):
         tree = BufferTree.tree_instance
@@ -144,7 +146,7 @@ class TreeNode:
         return len(self.buffer_blocks) > limit
 
     def is_root(self):
-        return BufferTree.tree_instance.root == self.dir_path
+        return BufferTree.tree_instance.root == self.node_timestamp
 
     def clear_internal_buffer(self):
         read_size = BufferTree.tree_instance.m
@@ -163,21 +165,31 @@ class TreeNode:
         pass
 
     def clear_leaf_buffer(self):
+        tree = BufferTree.tree_instance
+
         required_delete_from_outside = False
         num_children_before = len(self.children_paths)
 
-        # TODO Sort all buffer files into one big file
         sorted_file = ''
+        # TODO Sort all buffer files into one big file
+
+        with open(sorted_file, 'r') as file:
+            new_leaf_block = []
+
+            self.get_leaf_elements_as_list()
 
 
+            line = file.readline()
 
 
+            while True:
 
+                if not line:
+                    # TODO
+                    pass
+                buffer_element = buffer_element_from_sorted_file_line(line)
 
         return required_delete_from_outside
-
-    def chunks_of_data
-
 
     def pass_elements_to_children(self, elements):
         # Slightly complicated implementation, but therefore does not use unnecessary memory space
@@ -194,11 +206,18 @@ class TreeNode:
                     child_node_path = self.children_paths[child_index]
 
                     child_node = load_node(child_node_path)
-                    child_node.add_elements_to_buffer(self.dir_path, output_to_child)
+                    child_node.add_elements_to_buffer(self.node_timestamp, output_to_child)
                     write_node(child_node)
 
                 output_to_child = []
                 child_index += 1
+
+    def get_leaf_elements_as_list(self):
+        leaf_elements = []
+        for child in self.children_paths:
+            child_path = get_leaf_file_path_from_timestamps(self.node_timestamp, child)
+
+        pass
 
     @staticmethod
     def annihilate_insertions_deletions_with_matching_timestamps(elements):
@@ -250,9 +269,12 @@ class NodeBufferBlock:
 
 
 class BufferElement:
-    def __init__(self, element, action):
+    def __init__(self, element, action, timestamp=None, with_time=True):
+        if with_time and timestamp is None:
+            timestamp = get_current_timestamp()
+
         self.element = element
-        self.timestamp = datetime.now()
+        self.timestamp = timestamp
         self.action = action
 
 
@@ -265,7 +287,7 @@ class Action(str, Enum):
 
 
 # Node structure ideas:
-# is_internal_node, num_handles, handles, num_children, paths_to_children, num_buffer_blocks, paths_to_buffer_blocks
+# is_internal_node, num_handles, handles, num_children, *paths_to_children, num_buffer_blocks, *paths_to_buffer_blocks, size_of_last_buffer_block
 
 def load_node(node_dir_path, parent_path=None) -> TreeNode:
     # TODO
@@ -286,3 +308,19 @@ def read_buffer_block(block_path) -> NodeBufferBlock:
 def write_buffer_block(file_path, elements):
     # TODO
     pass
+
+
+# https://stackoverflow.com/questions/17444679/reading-a-huge-csv-file
+def chunks_of_data(data, chunk_size):
+    # TODO len(data) doesn't work on data from a csv reader. Need a slightly different approach if we want to read x lines
+
+    for i in range(0, chunk_size):
+        yield data[i: i+chunk_size]
+
+
+def buffer_element_from_sorted_file_line(line):
+    # TODO
+    ele = 'ele'
+    action = 'insert'
+
+    return BufferElement(ele, action, with_time=False)
