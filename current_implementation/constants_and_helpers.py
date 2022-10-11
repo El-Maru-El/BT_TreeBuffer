7daae455ee3d818c783a48dd7d9bdd594dc0f4ed
Maruan El-Mahgary <maruan.el-mahgary@gmx.de>
2022-10-11 17:32:49 +0200

    Update file-handling: Now only timestamps need to be stored. All file-paths can be re-constructed with only timestamp information

--- current_implementation/constants_and_helpers.py
+++ current_implementation/constants_and_helpers.py
@@ -9,46 +9,69 @@ WORKING_DIR = os.path.dirname(current_implementation.__file__)
 NODES_DIR = os.path.join(WORKING_DIR, 'nodes_collection')
 NODE_STRING = 'node_'
 TIMESTAMP_FORMAT = '%Y_%m_%d-%H_%M_%S_%f'
-BUFFERS_DIR_STRING = 'buffers'
 BLOCK_STRING = 'block_'
+LEAF_STRING = 'leaf_'
 # TODO Which file extension to use? CSV? TXT?
 FILE_EXTENSION = ''
 
 
+# Returns the timestamp, by which the rest can be reconstructed
 def generate_new_nodes_dir():
-    new_dir_name = nodes_dir_name_from_timestamp(get_current_timestamp())
-    new_node_dir = Path(os.path.join(NODES_DIR, new_dir_name))
-    buffer_dir_for_node = get_buffer_dir_for_node(new_node_dir)
+    timestamp = get_current_timestamp()
+    new_node_dir = get_node_dir_path_from_timestamp(timestamp)
 
-    buffer_dir_for_node.mkdir(parents=True, exist_ok=False)
+    new_node_dir.mkdir(parents=True, exist_ok=False)
 
-    return new_node_dir
+    return timestamp
 
 
+# Returns Node directory path
+def get_node_dir_path_from_timestamp(node_timestamp):
+    node_dir_name = nodes_dir_name_from_timestamp(node_timestamp)
+    return Path(os.path.join(NODES_DIR, node_dir_name))
+
+
+# Returns Node directory name
 def nodes_dir_name_from_timestamp(timestamp):
     return NODE_STRING + timestamp
-
-
-def get_buffer_dir_for_node(node_dir):
-    return Path(os.path.join(node_dir, BUFFERS_DIR_STRING))
 
 
 def get_current_timestamp():
     return datetime.now().strftime(TIMESTAMP_FORMAT)
 
 
-def delete_all_nodes():
-    shutil.rmtree(NODES_DIR)
-
-
-def generate_buffer_file_name():
-    return buffer_file_name_from_timestamp(get_current_timestamp())
-
-
+# Returns buffer file name
 def buffer_file_name_from_timestamp(timestamp):
     return BLOCK_STRING + timestamp()
+
+
+# Returns buffer file path
+def get_buffer_file_path_from_timestamps(node_timestamp, buffer_timestamp):
+    return Path(os.path.join(get_node_dir_path_from_timestamp(node_timestamp), buffer_file_name_from_timestamp(buffer_timestamp)))
 
 
 def write_buffer_block(file_path, elements):
     # TODO
     pass
+
+
+# Returns leaf file name
+def leaf_file_name_from_timestamp(timestamp):
+    return LEAF_STRING + timestamp()
+
+
+# Returns life file path
+def get_leaf_file_path_from_timestamps(node_timestamp, leaf_timestamp):
+    return Path(os.path.join(get_node_dir_path_from_timestamp(node_timestamp), leaf_file_name_from_timestamp(leaf_timestamp)))
+
+
+# https://stackoverflow.com/questions/6340351/iterating-through-list-of-list-in-python
+# Returns the elements of all lists within a super-list in order
+def traverse_lists_in_list(super_list):
+    for sublist in super_list:
+        for element in sublist:
+            yield element
+
+
+def delete_all_nodes():
+    shutil.rmtree(NODES_DIR)
