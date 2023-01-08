205922785e1147ab3eb9bb347f79e2d1d37e0a8b
Maruan El-Mahgary <maruan.el-mahgary@gmx.de>
2023-01-08 17:59:49 +0100

    Testing creating benchmark files

--- benchmarking/test_create_file/second_small_benchmark_file_.py
+++ benchmarking/test_create_file/second_small_benchmark_file_.py
@@ -9,37 +9,20 @@ class BigTestsWithTracking(unittest.TestCase):
     def setUp(self):
         clean_up_and_initialize_resource_directories()
 
-    def def_create_buffer_tree_benchmark(self, benchmark_name):
-
-        tree = self.create_buffer_tree()
-        biggest_int = 200 * tree.B * tree.m
-
-        elements = [create_string_from_int(i, biggest_int) for i in range(biggest_int)]
-
-        tree.start_tracking_handler()
-        for element in elements:
-            tree.insert_to_tree(element)
-
-        for element in elements[tree.B * 10:tree.B * 45]:
-            tree.delete_from_tree(element)
-
-        tree.flush_all_buffers()
-        tree.stop_tracking_handler(benchmark_name)
-
     def test_create_files_same_elements_different_trees(self):
         # Creating all the elements:
-        benchmark_name = 'big_big_big_test_benchmark'
-        delete_start_ind = 10240
-        delete_stop_ind = 46080
-        biggest_int = 1638400
+        benchmark_name = 'small_test_benchmark'
+        delete_start_ind = 1200
+        delete_stop_ind = 300
+        biggest_int = 4000
         elements = [create_string_from_int(i, biggest_int) for i in range(biggest_int)]
 
         # Buffer Tree:
         buffer_tree = self.create_buffer_tree()
         b_plus_tree = self.create_bplus_tree()
 
-        # trees = [buffer_tree, b_plus_tree]
-        trees = [b_plus_tree]
+        trees = [buffer_tree, b_plus_tree]
+        # trees = [b_plus_tree]
 
         for tree in trees:
             tree.start_tracking_handler()
